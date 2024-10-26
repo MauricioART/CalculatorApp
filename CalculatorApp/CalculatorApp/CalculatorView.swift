14270bd33de7172445e736e7cc783b071a9ae823
Mauricio Aguilera <artuaguila@gmail.com>
2024-10-25 18:29:11 -0600

    Add Constraints to buttons

--- CalculatorApp/CalculatorApp/CalculatorView.swift
+++ CalculatorApp/CalculatorApp/CalculatorView.swift
@@ -15,115 +15,115 @@ class CalculatorView: UIView {
       label.translatesAutoresizingMaskIntoConstraints = false
       label.text = "0"
       return label
-    }
+    }()
 
     lazy var ACButton: UIButton = {
-      let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("AC", for: .normal)
-        var configuration = UIButton.Configuration.plain()
-        configuration.cornerStyle = .large
-        configuration.background.backgroundColor = .systemCyan
-        configuration.baseForegroundColor = .systemGray6
-        button.configuration = configuration
+//      let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.setTitle("AC", for: .normal)
+//        var configuration = UIButton.Configuration.plain()
+//        configuration.cornerStyle = .large
+//        configuration.background.backgroundColor = .systemCyan
+//        configuration.baseForegroundColor = .systemGray6
+//        button.configuration = configuration
+        let button = createButton(text: "AC", color: .systemMint)
         return button
-    }
+    }()
 
     lazy var ansButton: UIButton = {
       let button = createButton(text: "ANS", color: .systemOrange)
       return button
-    }
+    }()
   
     lazy var eraseButton: UIButton = {
-      let button = createButton(text: "", color: .systemOrange)
+      let button = createButton(text: "DEL", color: .systemMint)
       return button
-    }
+    }()
 
     lazy var addButton: UIButton = {
       let button = createButton(text: "+", color: .systemOrange)
       return button
-    }
+    }()
 
-    lazy var subsButton: UIButton = 
+    lazy var subsButton: UIButton = {
       let button = createButton(text: "-", color: .systemOrange)
       return button
-      
-    }
+    }()
 
     lazy var mulButton: UIButton = {
       let button = createButton(text: "x", color: .systemOrange)
       return button      
-    }
+    }()
 
     lazy var divButton: UIButton = {
       let button = createButton(text: "/", color: .systemOrange)
       return button      
-    }
+    }()
 
     lazy var percentageButton: UIButton = {
       let button = createButton(text: "%", color: .systemOrange)
       return button      
-    }
+    }()
 
     lazy var operateButton: UIButton = {
       let button = createButton(text: "=", color: .systemOrange)
       return button      
-    }
+    }()
 
     lazy var oneButton: UIButton = {
       let button = createButton(text: "1", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var twoButton: UIButton = {
       let button = createButton(text: "2", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var threeButton: UIButton = {
       let button = createButton(text: "3", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var fourButton: UIButton = {
       let button = createButton(text: "4", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var fiveButton: UIButton = {
       let button = createButton(text: "5", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var sixButton: UIButton = {
       let button = createButton(text: "6", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var sevenButton: UIButton = {
       let button = createButton(text: "7", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var eightButton: UIButton = {
       let button = createButton(text: "8", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var nineButton: UIButton = {
       let button = createButton(text: "9", color: .systemGray)
       return button
-    }
+    }()
 
     lazy var zeroButton: UIButton = {
       let button = createButton(text: "0", color: .systemGray)
       return button
-    }  
+    }  ()
 
     lazy var pointButton: UIButton = {
       let button = createButton(text: ".", color: .systemGray)
       return button
-    }  
+    }  ()
 
     lazy var loginButton: UIButton = {
         let button = UIButton()
@@ -148,27 +148,29 @@ class CalculatorView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemBackground
-        addButtons()
         addViews()
         addContainerView()
         addAnswerLabel()
-        addButton()
+        addButtons()
+//        addButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func createButton(text: String, color: UIColor, image: UIImage?) -> UIButton{
+    private func createButton(text: String, color: UIColor) -> UIButton{
           let button = UIButton(type: .system)
-          button.frame = CGRect(x: 100, y: 100, width: 100, height: 100) // Tamaño cuadrado para asegurar forma circular
+          button.frame = CGRect(x: 100, y: 100, width: 80, height: 80) // Tamaño cuadrado para asegurar forma circular
           button.setTitle(text, for: .normal)
           button.backgroundColor = color
 
           // Redondear el botón para hacerlo circular
           button.layer.cornerRadius = button.frame.size.width / 2
           button.clipsToBounds = true // Asegura que el contenido siga la forma circular
-
+          button.translatesAutoresizingMaskIntoConstraints = false
+          button.restorationIdentifier = text
+          button.titleLabel!.font = UIFont.systemFont(ofSize: 24) 
           return button
     }
   
@@ -186,49 +188,65 @@ class CalculatorView: UIView {
     
     private func addAnswerLabel() {
         answerLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
-        answerLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8).isActive = true
-        answerLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8).isActive = true
+        answerLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 35).isActive = true
+        answerLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -35).isActive = true
+        answerLabel.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.15).isActive = true
+        answerLabel.text = "0"
+        answerLabel.backgroundColor = .systemBackground
+        answerLabel.font = .systemFont(ofSize: CGFloat(40), weight: .heavy)
+        answerLabel.textAlignment = .right
     }
     
-    private func addButton() {
-        loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 32).isActive = true
-        loginButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
-        loginButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
-        loginButton.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.8).isActive = true
-        let aspectRatio = loginButton.widthAnchor.constraint(equalTo: loginButton.heightAnchor, multiplier: 5)
-        aspectRatio.priority = .defaultHigh
-        aspectRatio.isActive = true
-    }
 
     private func addButtons(){
-      buttons.append(answerLabel)
-      buttons.append(ACButton)
-      buttons.append(ansButton)
-      buttons.append(percentageButton)
-      buttons.append(eraseButton)
-      buttons.append(sevenButton)
-      buttons.append(eightButton)
-      buttons.append(nineButton)
-      buttons.append(addButton)
-      buttons.append(fourButton)
-      buttons.append(fiveButton)
-      buttons.append(sixButton)
-      buttons.append(subsButton)
-      buttons.append(oneButton)
-      buttons.append(twoButton)
-      buttons.append(threeButton)
-      buttons.append(divButton)
-      buttons.append(pointButton)
-      buttons.append(zeroButton)
-      buttons.append(operateButton)
-      buttons.append(mulButton)
-
+        var buttonIndex = 0
+        for i in 0..<5{
+            for j in 0..<4{
+                if (i == 0){
+                    buttons[buttonIndex].topAnchor.constraint(equalTo: answerLabel.bottomAnchor, constant: 30).isActive = true
+                }else{
+                    buttons[buttonIndex].topAnchor.constraint(equalTo: buttons[4*(i-1)].bottomAnchor, constant: 5).isActive = true
+                }
+                if(j==0){
+                    buttons[buttonIndex].leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 25).isActive = true
+                }else{
+                    buttons[buttonIndex].leadingAnchor.constraint(equalTo: buttons[buttonIndex-1].trailingAnchor, constant: 10).isActive = true
+                }
+                buttons[buttonIndex].widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.2).isActive = true
+                buttons[buttonIndex].heightAnchor.constraint(equalTo: buttons[buttonIndex].widthAnchor, multiplier: 1).isActive = true
+                buttons[buttonIndex].tintColor = .black
+                buttonIndex = buttonIndex + 1
+            }
+        }
     }
+    
+    
     private func addViews() {
         addSubview(containerView)
+        containerView.addSubview(answerLabel)
+        buttons.append(ACButton)
+        buttons.append(ansButton)
+        buttons.append(percentageButton)
+        buttons.append(eraseButton)
+        buttons.append(sevenButton)
+        buttons.append(eightButton)
+        buttons.append(nineButton)
+        buttons.append(addButton)
+        buttons.append(fourButton)
+        buttons.append(fiveButton)
+        buttons.append(sixButton)
+        buttons.append(subsButton)
+        buttons.append(oneButton)
+        buttons.append(twoButton)
+        buttons.append(threeButton)
+        buttons.append(divButton)
+        buttons.append(pointButton)
+        buttons.append(zeroButton)
+        buttons.append(operateButton)
+        buttons.append(mulButton)
+
         for button in self.buttons {
           containerView.addSubview(button)
         }
-              
     }
 }
--- CalculatorApp/CalculatorApp/CalculatorViewController.swift
+++ CalculatorApp/CalculatorApp/CalculatorViewController.swift
@@ -9,29 +9,11 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
-    @IBOutlet weak asnwerLabel: UILabel = UILabel!
-    @IBOutlet weak ACButton: UIButton = UIButton!
-    @IBOutlet weak eraseButton: UIButton = UIButton!
-    @IBOutlet weak addButton: UIButton = UIButton!
-    @IBOutlet weak subsButton: UIButton = UIButton!
-    @IBOutlet weak mulButton: UIButton = UIButton!
-    @IBOutlet weak divButton: UIButton = UIButton!
-    @IBOutlet weak percentageButton: UIButton = UIButton!
-    @IBOutlet weak ansButton: UIButton = UIButton!
-    @IBOutlet weak operateButton: UIButton = UIButton!
-    @IBOutlet weak oneButton: UIButton = UIButton!
-    @IBOutlet weak twoButton: UIButton = UIButton!
-    @IBOutlet weak threeButton: UIButton = UIButton!
-    @IBOutlet weak fourButton: UIButton = UIButton!
-    @IBOutlet weak fiveButton: UIButton = UIButton!
-    @IBOutlet weak sixButton: UIButton = UIButton!
-    @IBOutlet weak sevenButton: UIButton = UIButton!
-    @IBOutlet weak eightButton: UIButton = UIButton!
-    @IBOutlet weak nineButton: UIButton = UIButton!
-    @IBOutlet weak zeroButton: UIButton = UIButton!
-    @IBOutlet weak pointButton: UIButton = UIButton!
     
     var answer: Float = 0.0
+    var operator1: Float = 0.0
+    var operator2: Float = 0.0
+    var display: String = "0"
     
     private var customView: CalculatorView {
         return view as! CalculatorView
@@ -43,19 +25,89 @@ class CalculatorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        customView.loginButton.addAction(UIAction(handler: doLogin(_:)), for: .touchUpInside)
+        //customView.loginButton.addAction(UIAction(handler: doLogin(_:)), for: .touchUpInside)
+        for button in customView.buttons{
+            button.addAction(UIAction(handler: doAction(_:)), for: .touchUpInside)
+        }
     }
     
-    @IBAction func doAction(_ sender: UIButton){
-        switch(sender.referenceIdentifier){
-            case "":
-            default: 
-                break
+    func doAction(_ sender: UIAction){
+        let button = sender.sender as! UIButton
+        switch(button.restorationIdentifier) {
+        case "AC":
+            cleanVariables()
+        case "ANS":
+            operator1 = answer
+        case "DEL":
+            display.removeLast()
+            updateDisplay()
+        case "/":
+            display.append("/")
+            updateDisplay()
+        case "x":
+            display.append("x")
+            updateDisplay()
+        case "+":
+            display.append("+")
+            updateDisplay()
+        case "-":
+            display.append("-")
+            updateDisplay()
+        case "=":
+            display.removeAll()
+            display.append("\(answer)")
+            updateDisplay()
+        case "%":
+            display.append("%")
+            updateDisplay()
+        case "0":
+            display.append("0")
+            updateDisplay()
+        case "1":
+            display.append("1")
+            updateDisplay()
+        case "2":
+            display.append("2")
+            updateDisplay()
+        case "3":
+            display.append("3")
+            updateDisplay()
+        case "4":
+            display.append("4")
+            updateDisplay()
+        case "5":
+            display.append("5")
+            updateDisplay()
+        case "6":
+            display.append("6")
+            updateDisplay()
+        case "7":
+            display.append("7")
+            updateDisplay()
+        case "8":
+            display.append("8")
+            updateDisplay()
+        case "9":
+            display.append("9")
+            updateDisplay()
+        default:
+            break
         }
+    }
+    
+    private func updateDisplay(){
+        customView.answerLabel.text = display
+    }
+    
+    private func cleanVariables(){
+        answer = 0.0
+        operator1 = 0.0
+        operator2 = 0.0
+        customView.answerLabel.text = "0"
     }
 
     private func updateAnswer(answer: String){
-        answerLabel.text = answer
+        customView.answerLabel.text = answer
     }
    
 
